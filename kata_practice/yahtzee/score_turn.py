86107107e8bdb0034c1b9c26a6c163d9038862ae
agware <alex.g.ware@gmail.com>
2024-03-01 20:32:36 +1100

    Add scoring method for any of a kind and associated tests

--- kata_practice/yahtzee/score_turn.py
+++ kata_practice/yahtzee/score_turn.py
@@ -1,8 +1,53 @@
-from typing import List
+from collections import Counter
+from typing import Callable, List, Optional
 
 from kata_practice.yahtzee.score_categories import ScoreCategoriesEnum
 
 
 def score_turn(rolls: List[int], category: ScoreCategoriesEnum) -> int:
-    assert len(rolls) == 5, "Expecting exactly 5 dice to have been rolled"
-    return 0
+    """Applies a scoring category to a set of rolls and returns the associated score"""
+    counted_rolls = Counter(rolls)
+    return get_scoring_method(category)(counted_rolls)
+
+
+def get_scoring_method(category: ScoreCategoriesEnum) -> Callable[[Counter], int]:
+    """A factory method to match a given scoring category to its method for scoring"""
+
+    if category == ScoreCategoriesEnum.THREE_OF_A_KIND:
+        return score_3_of_a_kind
+    elif category == ScoreCategoriesEnum.FOUR_OF_A_KIND:
+        return score_4_of_a_kind
+    elif category == ScoreCategoriesEnum.FIVE_OF_A_KIND:
+        return score_5_of_a_kind
+    else:
+        raise NotImplementedError
+
+
+def score_3_of_a_kind(counted_rolls: Counter) -> int:
+    return score_any_of_a_kind(counted_rolls, target=3)
+
+
+def score_4_of_a_kind(counted_rolls: Counter) -> int:
+    return score_any_of_a_kind(counted_rolls, target=4)
+
+
+def score_5_of_a_kind(counted_rolls: Counter) -> int:
+    return score_any_of_a_kind(counted_rolls, target=5, default_score=50)
+
+
+def score_any_of_a_kind(
+    counted_rolls: Counter, target: int, default_score: Optional[int] = None
+) -> int:
+    """
+    Checks whether there is a set of matching die values occuring at or more frequently than the target.
+    It will always use the most frequently occurring die value for the calculation
+    """
+    die_value, highest_count = counted_rolls.most_common()[0]
+
+    if highest_count < target:
+        return 0
+
+    if default_score is not None:
+        return default_score
+
+    return die_value * target
--- tests/test_yahtzee/test_scoring.py
+++ tests/test_yahtzee/test_scoring.py
@@ -1,11 +1,63 @@
+from typing import List
+
 import pytest
 
 from kata_practice.yahtzee.score_categories import ScoreCategoriesEnum
 from kata_practice.yahtzee.score_turn import score_turn
 
 
+@pytest.mark.xfail(
+    reason="Scoring methods haven't been implemented for all categories yet"
+)
 @pytest.mark.parametrize("category", ScoreCategoriesEnum)
 def test_all_score_categories_return_a_value(category: ScoreCategoriesEnum):
     dice_rolls = [1, 1, 1, 1, 1]
     result = score_turn(dice_rolls, category)
     assert result is not None
+
+
+@pytest.mark.parametrize(
+    ("dice_rolls", "expected_score"),
+    (
+        ([1, 2, 3, 4, 5, 6, 7], 0),
+        ([2, 3, 2, 3, 5], 0),
+        ([1, 1, 1], 3),
+        ([-2, 1, -2, -2], -6),
+        ([6, 6, 6, 5, 6, 5], 18),
+        ([5, 5, 5, 5, 5], 15),
+    ),
+)
+def test_score_3_of_a_kind(dice_rolls: List[int], expected_score: int):
+    result = score_turn(dice_rolls, ScoreCategoriesEnum.THREE_OF_A_KIND)
+    assert result == expected_score
+
+
+@pytest.mark.parametrize(
+    ("dice_rolls", "expected_score"),
+    (
+        ([1, 2, 3, 4, 5, 6, 7], 0),
+        ([2, 3, 2, 3, 2], 0),
+        ([1, 1, 1, 1], 4),
+        ([2, 1, 2, 2, 2], 8),
+        ([6, 6, 6, 5, 6, 5], 24),
+        ([5, 5, 5, 5, 5], 20),
+    ),
+)
+def test_score_4_of_a_kind(dice_rolls: List[int], expected_score: int):
+    result = score_turn(dice_rolls, ScoreCategoriesEnum.FOUR_OF_A_KIND)
+    assert result == expected_score
+
+
+@pytest.mark.parametrize(
+    ("dice_rolls", "expected_score"),
+    (
+        ([1, 2, 3, 4, 5, 6, 7], 0),
+        ([2, 3, 2, 3, 2], 0),
+        ([6, 6, 6, 5, 6, 5], 0),
+        ([5, 5, 5, 5, 5], 50),
+        ([4, 4, 4, 4, 4], 50),
+    ),
+)
+def test_score_5_of_a_kind(dice_rolls: List[int], expected_score: int):
+    result = score_turn(dice_rolls, ScoreCategoriesEnum.FIVE_OF_A_KIND)
+    assert result == expected_score
